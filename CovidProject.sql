79df5b1f83dbeaca2ea0a2daef966e14cffc8783
GregoryGreen <87345933+GregoryGreen@users.noreply.github.com>
2022-07-28 14:08:24 -0400

    Update CovidProject.sql

--- CovidProject.sql
+++ CovidProject.sql
@@ -78,7 +78,7 @@ order by TotalDeathCount desc
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths  as int))/SUM(New_Cases * 1.0)*100 as DeathPercentage
 From CovidDeaths
 --Where location like '%states%'
-where continent is not null 
+where continent <> '' 
 --Group By date
 order by 1,2;
 
